976e2324670c4636165a504f7d9f0d72720bf344
Molina Thibaut <thibautmolina@yahoo.fr>
2020-02-04 22:55:44 +0100

    generation pdf angular

--- RecetteCuisine/src/app/services/pdf.service.ts
+++ RecetteCuisine/src/app/services/pdf.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { PdfMakeWrapper, Img } from 'pdfmake-wrapper';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +10,22 @@ import { HttpClient } from '@angular/common/http';
 export class PdfService {
 
   private url: string;
+  private pdf = new PdfMakeWrapper();
 
   constructor(private http: HttpClient) {
     this.url = environment.urldatabase + 'pdf';
   }
 
-  createPDF(str: string) {
+  async createPDF(str: string) {
+    PdfMakeWrapper.setFonts(pdfFonts);
     const data = { message: str };
-    this.http.post(this.url, data).subscribe((rep) => {
+    /*this.http.post(this.url, str).subscribe((rep) => {
       console.log(rep);
-    });
+    });*/
+    this.pdf.add(str);
+    const image=new Img('assets/cuisine.jpg').build();
+    this.pdf.add(await image);
+    this.pdf.create().download();
+
   }
 }
